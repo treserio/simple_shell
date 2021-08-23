9ecf7d81c589b8993503c3c1be0a1ba812f0b247
root <NicholasJWallace@protonmail.com>
2021-08-23 11:46:22 -0700

    prototype

---
+++ strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - appends the src string to the dest string
+ * @s1: destination char
+ * @s2: source char
+ * Return: comparison
+ **/
+int _strcmp(char *s1, char *s2)
+{
+  int num = 0, i = 0;
+
+  while (s1[i] != '\0' || s2[i] != '\0')
+    {
+      if (s1[i] != s2[i])
+	{
+	  num = 1;
+	  return (num);
+	}
+      i++;
+    }
+  return (num);
+}
